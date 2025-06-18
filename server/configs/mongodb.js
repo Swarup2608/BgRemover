00bb4371ebd68a04cad89685583adafad9f02e2f
Swarup2608 <swarupmantripragada@email.com>
2025-06-18 15:49:01 +0530

    error console on connect

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -4,6 +4,9 @@ const connectDB = async () =>{
     mongoose.connection.on('connected',()=>{
         console.log("Connected with DB!");
     })
+     mongoose.connection.on("error", (err) => {
+      console.error("❌ MongoDB connection error:", err);
+    });
     await mongoose.connect(`${process.env.MONGODB_URI}/bgRemover`);
 }
 
