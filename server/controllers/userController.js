9e42bcfb80b22fa0d9986d9ceaefb8f46263d56b
Swarup2608 <swarupmantripragada@email.com>
2025-06-18 15:32:19 +0530

    webhooks added

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,5 @@
 import { Webhook } from "svix"
+import userModel from "../models/userModel.js"
 
 // API Controller Function to manage Clerk User with DB:
 // http://localhost:4000/api/user/webHook
@@ -6,8 +7,59 @@ import { Webhook } from "svix"
 const clerkWebHooks = async (req,res) =>{
     try {
         //create svix instance with clerk web hook
-        const webHook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
+        const webHook = new Webhook(process.env.CLERK_WEBHOOK_SECRET_KEY)
+        await webHook.verify(JSON.stringify(req.body),{
+            "svix-id":req.headers["svix-id"],
+            "svix-timestamp":req.headers["svix-timestamp"],
+            "svix-signature":req.headers["svix-signature"]
+        })
+
+        const {data,type} = req.body
+        switch(type){
+            case "user.created":{
+                const userData = {
+                    clerkId :  data.id,
+                    email : data.email_addresses[0].email_address,
+                    firstName : data.first_name,
+                    lastName : data.last_name,
+                    photo : data.image_url
+                }
+
+                await userModel.create(userData);
+                res.json({})
+                break;
+            }
+
+            case "user.updated":{
+                const userData = {
+                    email : data.email_addresses[0].email_address,
+                    firstName : data.first_name,
+                    lastName : data.last_name,
+                    photo : data.image_url
+                }
+
+                await userModel.findOneAndUpdate({clerkId: data.id},userData);
+                res.json({})
+
+                break;
+            }
+
+            case "user.deleted":{
+                await userModel.findOneAndDelete({clerkId:data.id});
+                res.json({})
+                break;
+            }
+
+            default:
+                break;
+
+        }
+
+
     } catch (error) {
-        
+        console.log(error.message);
+        res.json({success:false,message:error.message})
     }
 }
+
+export {clerkWebHooks}
